fd8980b90dea99f56e7d94b7679b53c9c37d35c4
huxiaoou <516984451@qq.com>
2022-07-01 09:44:18 +0800

    Reset argument batch for 05_convert_to_xuntou_B_diff.py to 1

--- run_on_execute_date_manually.ps1
+++ run_on_execute_date_manually.ps1
@@ -4,5 +4,5 @@ $factor = Read-Host -Prompt "Please Input the factor, available options = [RSW25
 python 02_revised_position.py $trade_date $factor >> .\log\$trade_date.manual.log
 python 03_position_diff.py $trade_date $factor >> .\log\$trade_date.manual.log
 python 04_convert_to_xuntou_A_group.py $trade_date $factor >> .\log\$trade_date.manual.log
-python 05_convert_to_xuntou_B_diff.py $trade_date $factor 2 >> .\log\$trade_date.manual.log
+python 05_convert_to_xuntou_B_diff.py $trade_date $factor 1 >> .\log\$trade_date.manual.log
 Pause
